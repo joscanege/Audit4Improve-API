25f9fb7d386aff494b6c306bd5e9510d45c1d65d
joscanege <jamacande.jmc@outlook.es>
2024-04-10 10:17:47 +0100

    Prueba UwU de rama nueva

---
+++ src/test/java/us/muit/fs/a4i/test/control/testRandom.java
@@ -0,0 +1,43 @@
+package us.muit.fs.a4i.test.control;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.atLeast;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import us.muit.fs.a4i.control.IndicatorsCalculator;
+import us.muit.fs.a4i.model.entities.Indicator;
+import us.muit.fs.a4i.model.entities.Report;
+import us.muit.fs.a4i.model.entities.ReportI;
+import us.muit.fs.a4i.model.entities.ReportItem;
+import us.muit.fs.a4i.model.entities.ReportItemI;
+
+/**
+ * <p>
+ * Test para probar la creación de tests
+ * </p>
+ * 
+ * @author JMCandilejo
+ *
+ */
+@ExtendWith(MockitoExtension.class)
+public class testRandom {
+}
